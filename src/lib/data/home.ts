c6e5b42b878abf8e0a66480a209956f1a8452c43
swrobel491 <swrobel491@gmail.com>
2024-10-23 10:18:11 -0400

    fix

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -11,11 +11,7 @@ export const description =
 	'A results-driven Senior .NET Engineer with 10+ years of expertise in architecting and developing high-performance, scalable systems in the FinTech, InsurTech, and healthcare sectors.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
-	{
-		platform: Platform.StackOverflow,
-		link: 'https://stackoverflow.com/'
-	},
+	{ platform: Platform.GitHub, link: 'https://github.com/kevinrozedev' },
 	{
 		platform: Platform.Email,
 		link: 'kevin.rozedev@gmail.com'
